344cb5eb9e45572c22692af68d3d62b7280573d0
zbshadow <zbshadowx@gmail.com>
2020-06-21 19:55:14 -0400

    Enhcanced Form to handle customer Type Added Discount Amounts by custmoer type Created required Fields Restructured Invoice Total Folder

--- InvoiceTotal/Program.cs
+++ InvoiceTotal/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +16,7 @@ namespace InvoiceTotal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmInvoiceTotal());
+            Application.Run(new Form1());
         }
     }
 }
